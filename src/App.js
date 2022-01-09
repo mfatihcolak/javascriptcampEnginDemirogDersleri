6725b8c330ebaadb4abdfb8f4a99cdd1d0107dca
fatihcolak20 <pardusanatolya@gmail.com>
2022-01-09 04:22:26 +0300

    İlk deneme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      
+
     </div>
   );
 }
 
 export default App;
+
+//JS TYPESAFE değildir
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+  let dolarDun = 9.10
+}
+
+console.log(dolarBugun);
+console.log(dolarDun);
+
+const euroDun = 11.2
+
+console.log(euroDun);
+
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>");
+for(let i = 0; i<konutKredileri.length;i++){
+    console.log("<li> " + konutKredileri[i] + " </li>");
+}
+console.log("</ul>");
+
+
